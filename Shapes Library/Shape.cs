19708b018f5af54184e4c7ed82e110cac562e56b
Zayon84 <dalmkvist@hotmail.com>
2022-09-28 15:21:56 +0200

    Added all classes for the Library

---
+++ Shapes Library/Shape.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Shapes_Library
+{
+    public abstract class Shape
+    {
+        public abstract Vector3 Center { get; }
+        public abstract float Area { get; }
+    }
+
+}
